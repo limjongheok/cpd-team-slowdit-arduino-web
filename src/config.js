398f4129fdd436e5bab67722570762184d337b87
lim-jongheok <20180719gm.hannam.ac.kr>
2022-06-05 03:58:09 +0900

    config 수정

--- src/config.js
+++ src/config.js
@@ -6,17 +6,18 @@ import { getDatabase } from "firebase/database";
 
 
 var myconfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATA_BASEURL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_FIREBASE_APPID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
+    apiKey: "process.env.REACT_APP_FIREBASE_APIKEY",
+    authDomain: "process.env.REACT_APP_FIREBASE_AUTH_DOMAIN",
+    databaseURL: "https://cpd-team-slowdit-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "process.env.REACT_APP_FIREBASE_PROJECTID,",
+    storageBucket: "process.env.REACT_APP_FIREBASE_STORAGEBUCKET",
+    messagingSenderId: "process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID",
+    appId: "process.env.REACT_APP_FIREBASE_APPID",
+    measurementId: "process.env.REACT_APP_FIREBASE_MEASUREMENTID"
 };
 
 
 
 const app = initializeApp(myconfig);
+
 export default app;
