ee61fc94f6ad5781cbaeec162927d2e0b48d37f0
lim-jongheok <20180719gm.hannam.ac.kr>
2022-06-10 04:51:55 +0900

    오디오 추가

--- src/App.js
+++ src/App.js
@@ -2,18 +2,28 @@ import firebase from "./config";
 import React, {useState, useEffect} from "react";
 import { getDatabase, ref, onValue ,set} from "firebase/database";
 import styles from './App.module.css'
+import music from './mp3/sound.mp3'
 
 function App() {
   const [count, setCount] = useState();
+  let au = new Audio(music)
+  
+  
+  function start(){
+    au.play();
+  }
   
 
   useEffect(() => {
     
-
+    
+    start();
+  
     const database = getDatabase(firebase)
     const starref = ref(database,'한남대 디자인팩토리/count') // 위치는 맞다 
-    
+  
     var data;
+  
     onValue(starref,(snapshot)=>{
       data = snapshot.val();
       setCount(data)
@@ -30,8 +40,7 @@ function App() {
   return (
     <div className="App">
       <div className={styles.background}><p className={styles.font}>{count}</p></div>
-      
-    
+
     </div>
   );
 }
