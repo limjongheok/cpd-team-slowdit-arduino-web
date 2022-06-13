910e7e433cf1a42bc587914d63115e83a5094fc5
lim-jongheok <20180719gm.hannam.ac.kr>
2022-06-13 20:25:34 +0900

    geolaction

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import firebase from "./config";
+import{db,app} from "./config";
 import React, {useState, useEffect} from "react";
 import { getDatabase, ref, onValue ,set} from "firebase/database";
 import styles from './App.module.css'
 import music from './mp3/sound.mp3'
+import {doc,getDocs,setDoc,collection} from "firebase/firestore"
 
 function App() {
   const [count, setCount] = useState();
   let au = new Audio(music)
   
-  
+  const refs = collection(db,"users")
   function start(){
     au.play();
+  }
+  const getUsers = async () =>{
+    const data = await getDocs(refs);
+    console.log(data);
   }
   
 
   useEffect(() => {
     
     
-    start();
   
-    const database = getDatabase(firebase)
+    const database = getDatabase(app)
     const starref = ref(database,'한남대 디자인팩토리/count') // 위치는 맞다 
   
     var data;
@@ -35,6 +39,32 @@ function App() {
   
 
   },[count])
+
+  useEffect(() => {
+
+    if (navigator.geolocation) {
+    
+      // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+      navigator.geolocation.getCurrentPosition(function(position) {
+        
+
+        var lat = position.coords.latitude; // 위도
+        var lon = position.coords.longitude; // 경도
+    
+        const userref = doc(db,'users','한남대 디자인팩토리');
+        setDoc(userref,{lat:lat,lon:lon});
+
+          
+          
+          
+              
+        });
+      
+  } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+      console.log("gps 사용 불가")
+      
+  }
+  },[])
 
   
   return (
--- src/config.js
+++ src/config.js
@@ -1,23 +1,23 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
+
 import { getFirestore } from "firebase/firestore";
-import { getDatabase } from "firebase/database";
 
 
 var myconfig = {
-    apiKey: "process.env.REACT_APP_FIREBASE_APIKEY",
-    authDomain: "process.env.REACT_APP_FIREBASE_AUTH_DOMAIN",
-    databaseURL: "https://cpd-team-slowdit-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "process.env.REACT_APP_FIREBASE_PROJECTID,",
-    storageBucket: "process.env.REACT_APP_FIREBASE_STORAGEBUCKET",
-    messagingSenderId: "process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID",
-    appId: "process.env.REACT_APP_FIREBASE_APPID",
-    measurementId: "process.env.REACT_APP_FIREBASE_MEASUREMENTID"
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: "https://cpd-team-slowdit-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
 };
 
 
 
-const app = initializeApp(myconfig);
 
-export default app;
+
+
+export const app =initializeApp(myconfig);
+export const db = getFirestore(app);
